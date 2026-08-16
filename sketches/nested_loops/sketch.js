8a9ca159aec8ce13aa7cd732fea86fabe8be5202
Pascal Bompard <pascal@Pascals-MacBook-Pro.local>
2026-08-17 06:49:48 +0800

    Nested loops creating carp scales

--- sketches/nested_loops/sketch.js
+++ sketches/nested_loops/sketch.js
@@ -1,21 +1,28 @@
 let spacing = 100;
 let x0 = spacing/2;
 let y0 = spacing/2;
-let size = 200;
-let columns
-let rows
-
+let size = spacing * 2;
+let columns;
+let rows;
+let colourRampSpeed;
 
 function setup() {
-    createCanvas(800, 600);
-    background(220);
-    columns = width / spacing;
-    rows = height / spacing;
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+    stroke('rgba(0, 0, 0, 0.1)');
+    strokeWeight(2);
+    columns = ceil(width / spacing);
+    rows = ceil(height / spacing);
+    colourRampSpeed = 185 / (columns + rows);
     print("columns = " + columns + "  rows = " + rows);
+    print("colourRampSpeed = " + colourRampSpeed);
 
     for (let i = 0; i < columns; i++) {
         for (let j = 0; j < rows; j++) {
-            fill(20*i + 20*j);
+            fill(
+                100 + colourRampSpeed * i + colourRampSpeed * j, 
+                30 + colourRampSpeed * i + colourRampSpeed * j, 
+                1 + colourRampSpeed * i + colourRampSpeed * j);
             ellipse(x0 + i * spacing, y0 + j * spacing, size, size);
         }
     }
