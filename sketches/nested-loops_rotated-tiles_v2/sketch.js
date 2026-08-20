6b1b844d40ed13b0882f30d2a3ee871c090ba116
Pascal Bompard <pascal.bompard@gmail.com>
2026-08-21 07:13:17 +0800

    added colour cycling

--- sketches/nested-loops_rotated-tiles_v2/sketch.js
+++ sketches/nested-loops_rotated-tiles_v2/sketch.js
@@ -6,20 +6,19 @@ The `setup` function initializes the canvas and draws the ellipses in the specif
 let spacing; //spacing between the ellipses in the grid, will be set based on the selected size mode
 let columns; //number of columns in the grid, will be calculated based on the canvas width and spacing
 let rows; //number of rows in the grid, will be calculated based on the canvas height and spacing
-let colourRampKoi = [100, 30, 1]; //starting color for the Koi color ramp, will be adjusted based on the position of the ellipses in the grid
-let colourRampPurple = [20, 0, 40];
-let colourRampSpeed; //speed at which the color changes across the grid, will be calculated based on the number of columns and rows
-let modeColourOptions = ["BW", "Koi", "Purple"]; //available color modes
-let modeColour = modeColourOptions[0]; //Toggle between modes
+let saturationRamp;
+let brightnessRamp; 
 let modeSizeOptions = ["Small", "Medium", "Large"]; //available size modes
 let modeSize = modeSizeOptions[0]; //Toggle between modes
 let x0;
 let y0;
 let size;
-
+let hueIndex = 0;
+let hueFactor = 1; //allows for cycling through the color ramps, will be incremented in the draw function to create a dynamic effect
 
 function setup() {
     angleMode(DEGREES);
+    colorMode(HSB);
     rectMode(CENTER);
     createCanvas(windowWidth, windowHeight); //create a canvas that fills the window
 
@@ -40,20 +39,31 @@ function setup() {
     rows = ceil(height / spacing); //calculate the number of rows based on the canvas height and spacing
 
     
-    colourRampSpeed = 255 / (columns + rows); 
+    saturationRamp = 5;
+    brightnessRamp = 2;
+    //print(saturationRamp);
 }
     
 
 
 function draw() {
     background(0);
-    stroke(0, 150);
-    //noStroke();
+    stroke(0, 150)
+    if (hueIndex > 98) {
+        hueFactor = -1;
+    } else if (hueIndex < 2) {
+        hueFactor = 1;
+    };
+    hueIndex = hueIndex + 0.1 * hueFactor; //allow for cycling with a boomerang effect
+    print(hueFactor);
+
+    
     for (let i = 0; i < columns+2; i++) {
         for (let j = 0; j < rows+3; j++) {
-            fill(0 + colourRampSpeed * i + colourRampSpeed * j);
+            fill(hueIndex,saturationRamp * i, brightnessRamp * j);
             let x = x0 + i * spacing; //calculate the x position of the ellipse based on its column index and spacing
             let y = y0 + j * spacing; //calculate the y position of the ellipse based on its row index and spacing
+            
             push();
                 translate(x,y)
                 rotate(frameCount * 0.05); //rotate the square based on its position in the grid to create a dynamic effect
